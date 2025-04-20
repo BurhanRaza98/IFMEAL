4107521be2768ae8ad7ba70ef7ebb67313cce0a2
Burhan Raza <burhanraza98@gmail.com>
2025-04-20 19:33:04 +0200

    Initial Commit

---
+++ IFMEALTests/IFMEALTests.swift
@@ -0,0 +1,17 @@
+//
+//  IFMEALTests.swift
+//  IFMEALTests
+//
+//  Created by Burhan Raza on 20/04/25.
+//
+
+import Testing
+@testable import IFMEAL
+
+struct IFMEALTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
